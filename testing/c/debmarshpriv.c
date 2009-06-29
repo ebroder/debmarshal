b323b336dafdfe8b7083bf955bff7e6863da9982
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-29 20:41:46 +0000

    Harden debmarshpriv a bit more by ignoring environment variables and running Python from an empty temporary directory.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@56 485416fb-281f-0410-a025-fb7d5554b9fe

--- testing/c/debmarshpriv.c
+++ testing/c/debmarshpriv.c
@@ -6,21 +6,41 @@
 #include "config.h"
 
 int main(int argc, char **argv) {
-  // argc + 3 because we're adding 3 arguments, removing argv[0], and
+  // cd into a newly created directory so that modules in the CWD
+  // can't affect execution
+  char tmpdir[40] = "/tmp/debmarshal.tmp.XXXXXX";
+  if (!mkdtemp(tmpdir)) {
+    perror("debmarshpriv");
+    exit(1);
+  }
+  if (-1 == chdir(tmpdir)) {
+    perror("debmarshpriv");
+    exit(1);
+  }
+  if (-1 == rmdir(tmpdir)) {
+    perror("debmarshpriv");
+    exit(1);
+  }
+
+  // argc + 4 because we're adding 4 arguments, removing argv[0], and
   // allocating space for the NULL at the end
-  char **new_argv = malloc(sizeof(char *) * (argc + 3));
+  char **new_argv = malloc(sizeof(char *) * (argc + 4));
   if (!new_argv) {
     perror("debmarshpriv");
     exit(1);
   }
-  new_argv[0] = "python";
-  new_argv[1] = "-m";
-  new_argv[2] = "debmarshal.privops";
 
+  new_argv[0] = "python";
+  // Ignore all environment variables that can affect the interpreter
+  // (e.g. PYTHONPATH)
+  new_argv[1] = "-E";
+  new_argv[2] = "-m";
+  new_argv[3] = "debmarshal.privops";
   // Skip argv[0], because we don't want to pass that along
   //
   // Also, argv[argc] is already NULL, so we'll just copy that over
-  memcpy(new_argv + 3, argv + 1, sizeof(char *) * argc);
+  memcpy(new_argv + 4, argv + 1, sizeof(char *) * argc);
+
   if (execv(PYTHON, new_argv)) {
     perror("debmarshpriv");
     exit(1);
