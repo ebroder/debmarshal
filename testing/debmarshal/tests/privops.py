ce68d62ec7f41c770dc556b6ce66796a5dbc7b93
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-29 20:37:43 +0000

    Write a test for debmarshal.privops.runWithPrivileges to make sure that, if os.geteuid returns 0, the function is just directly executed.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@23 485416fb-281f-0410-a025-fb7d5554b9fe

--- testing/debmarshal/tests/privops.py
+++ testing/debmarshal/tests/privops.py
@@ -7,13 +7,35 @@ __authors__ = [
 
 
 import os
+import subprocess
 import sys
 import unittest
 import mox
+from debmarshal import privops
 
 
 class TestRunWithPrivilege(mox.MoxTestBase):
-  pass
+  def testFunctionCalledDirectlyAsRoot(self):
+    self.mox.StubOutWithMock(os, 'geteuid')
+    os.geteuid().AndReturn(0)
+
+    # We want to make sure that runWithPrivilege doesn't try to
+    # re-execute itself through the setuid wrapper.
+    #
+    # By stubbing out os.stat without actually recording any calls to
+    # it, we can trigger an error of runWithPrivilege starts trying to
+    # re-execute itself.
+    self.mox.StubOutWithMock(os, 'stat')
+    self.mox.StubOutWithMock(subprocess, 'Popen')
+
+    self.mox.ReplayAll()
+
+    @privops.runWithPrivilege('test')
+    def func():
+      # Stupid function - all we care about is that it gets run
+      return 1
+
+    self.assertEqual(func(), 1)
 
 
 if __name__ == '__main__':
