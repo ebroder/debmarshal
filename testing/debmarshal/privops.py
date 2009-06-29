f2a70904b2fecdfc87879aef031d934a2ffa48fa
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-29 20:36:56 +0000

    Implement debmarshal.privops.runWithPrivilege, a decorator that will transparently caused wrapped functions to be re-execed as root through a setuid wrapper.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@15 485416fb-281f-0410-a025-fb7d5554b9fe

--- testing/debmarshal/privops.py
+++ testing/debmarshal/privops.py
@@ -19,3 +19,87 @@ the future.
 __authors__ = [
     'Evan Broder <ebroder@google.com>',
 ]
+
+
+import os
+import subprocess
+import decorator
+import yaml
+from debmarshal import errors
+
+
+# I really wish I could somehow incorporate a PREFIX or libexecdir or
+# something, but Python doesn't really want to export any of those
+# through distutils/setuptools
+_SETUID_BINARY = '/usr/lib/debmarshal/debmarshal-privops'
+
+
+_subcommands = {}
+
+
+def runWithPrivilege(subcommand):
+  """Decorator for wrapping a function to ensure that it gets run with
+  privileges by using a small setuid wrapper binary.
+
+  If a function wrapped with runWithPrivilege is called by a non-root
+  user, execute the setuid wrapper with the arguments passed in.
+
+  If a function is re-executed through the setuid wrapper, the
+  function arguments and keyword arguments are passed in through the
+  command line in YAML.
+
+  For security reasons, all YAML dumps and loads occur using the
+  "safe" parser, which will only (de-)serialize built-in types. This
+  means that arguments to and return values from functions wrapped in
+  runWithPrivilege must be limited to built-ins.
+
+  The return value or raised exceptions of the function are also
+  passed from the setuid subprocess back to the caller via standard
+  out, so functions wrapped in runWithPrivilege shouldn't print
+  anything.
+
+  Args:
+    subcommand: This is used as the first argument to the setuid
+      binary. Since the setuid binary simply executes this module, the
+      subcommand is also tracked internally for dispatching
+  """
+  def _runWithPrivilege(f, *args, **kwargs):
+
+    # If we already have our privileges
+    if os.geteuid() == 0:
+      return f(*args, **kwargs)
+    else:
+
+      # Make sure that the setuid binary is actually setuid root so
+      # we don't get stuck in a loop
+      stats = os.stat(_SETUID_BINARY)
+      if not (stats.st_mode & 04000 and stats.st_uid == 0):
+        raise errors.Error('%s is not setuid root' % _SETUID_BINARY)
+
+      p = subprocess.Popen([_SETUID_BINARY,
+                            subcommand,
+                            yaml.safe_dump(args),
+                            yaml.safe_dump(kwargs)],
+                           stdin=None,
+                           stdout=subprocess.PIPE,
+                           close_fds=True)
+      rc = p.wait()
+
+      # This is the only place we don't use yaml.safe_load. That's
+      # intentional, because the source of this string is trusted, and
+      # may be an object like an exception.
+      ret = yaml.load(p.stdout)
+      if not rc:
+        raise ret
+      else:
+        return ret
+
+  # The extra layer of redirection is needed if we want to both (a)
+  # use the decorator module (we do, because it gives us nice
+  # function-signature-preserving properties) and (b) associate a
+  # subcommand with the function it's wrapping at parse time.
+  def _makeRunWithPriv(f):
+    _subcommands[subcommand] = f
+    return decorator.decorator(_runWithPrivilege, f)
+
+  return _makeRunWithPriv
