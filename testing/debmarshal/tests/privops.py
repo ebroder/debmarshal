4d7c955fa8e7cf51e1da9477f162b4f6066fd8de
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-29 20:37:37 +0000

    Start to stub out tests for debmarshal.privops.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@22 485416fb-281f-0410-a025-fb7d5554b9fe

---
+++ testing/debmarshal/tests/privops.py
@@ -0,0 +1,20 @@
+# -*- python-indent: 2 -*-
+"""tests for the debmarshal setuid support module"""
+
+__authors__ = [
+    'Evan Broder <ebroder@google.com>',
+]
+
+
+import os
+import sys
+import unittest
+import mox
+
+
+class TestRunWithPrivilege(mox.MoxTestBase):
+  pass
+
+
+if __name__ == '__main__':
+  unittest.main()
