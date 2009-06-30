adcd04778706398eda9bfa7d606d1416ef173858
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-30 22:15:16 +0000

    Create a debmarshals.hypervisors.base package with a Hypervisor class for representing hypervisors and their configuration.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@74 485416fb-281f-0410-a025-fb7d5554b9fe

---
+++ testing/debmarshal/hypervisors/base.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- python-indent: 2; py-indent-offset: 2 -*-
+# Copyright 2009 Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+"""Base for debmarshal's hypervisor representations."""
+
+
+__authors__ = [
+  'Evan Broder <ebroder@google.com>',
+]
+
+
+import os
+import stat
+
+from lxml import etree
+
+
+def _diskIsBlockDevice(disk):
+  """Identify whether a particular disk is an image file or block
+  device
+
+  Args:
+    disk: Path to the disk image being tested
+
+  Returns:
+    True if the disk is a block device; False if it's a file
+  """
+  return stat.S_ISBLK(os.stat(disk).st_mode)
+
+
+class Hypervisor(object):
+  """Superclass representation of an abstract hypervisor used to run
+  debmarshal tests.
+
+  Classes derived from Hypervisor represent virtual machine
+  hypervisors capable of creating virtual domains. They also have a
+  hand in the runtime configuration of such hypervisors.
+  """
+  @staticmethod
+  def domainXML(vm):
+    """Generate the XML to pass to libvirt to create a new domain.
+
+    This will generate the portions of the XML that are common to all
+    hypervisors; subclasses of Hypervisor will add on elements and
+    properties to the XML tree returned from their superclass.
+
+    Args:
+      vm: an instance of debmarshal.vm.VM with the parameters for the
+        VM
+
+    Returns:
+      An lxml.etree.Element object containing the parsed XML tree.
+    """
+    xml = etree.Element('domain')
+    etree.SubElement(xml, 'name').text = vm.name
+    etree.SubElement(xml, 'memory').text = str(vm.memory)
+
+    devices = etree.SubElement(xml, 'devices')
+    for disk_num, disk in enumerate(vm.disks):
+      xml_disk = etree.SubElement(devices, 'disk')
+
+      if _diskIsBlockDevice(disk):
+        xml_disk.set('type', 'block')
+        etree.SubElement(xml_disk, 'source', dev=disk)
+      else:
+        xml_disk.set('type', 'file')
+        etree.SubElement(xml_disk, 'source', file=disk)
+
+      disk_letter = chr(disk_num + ord('a'))
+      etree.SubElement(xml_disk, 'target', dev='sd%s' % disk_letter)
+
+    xml_net = etree.SubElement(devices, 'interface')
+    etree.SubElement(xml_net, 'source', network=vm.network)
+    etree.SubElement(xml_net, 'mac', address=vm.mac)
+
+    return xml
