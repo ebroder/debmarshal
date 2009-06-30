b1cc9837384109b6b33449d23e2ec344534a97d9
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-30 23:18:08 +0000

    Stub out debmarshal.privops.domains.createDomain.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@88 485416fb-281f-0410-a025-fb7d5554b9fe

--- testing/debmarshal/privops/domains.py
+++ testing/debmarshal/privops/domains.py
@@ -28,3 +28,39 @@ installing OS images.
 __authors__ = [
   'Evan Broder <ebroder@google.com>',
 ]
+
+
+from debmarshal import errors
+from debmarshal.privops import utils
+
+
+@utils.runWithPrivilege('create-domain')
+def createDomain(memory, disks, network, mac, hypervisor="qemu"):
+  """Create a virtual machine domain.
+
+  createDomain creates a domain for a virtual machine used as part of
+  a debmarshal test and boots it up.
+
+  We do no validation or accounting of memory allocations from the
+  privileged side. Since debmarshal is intended to be run on
+  single-user machines, the worst case scenario is a DoS of yourself.
+
+  Args:
+    memory: str containing the amount of memory to be allocated to the
+      new domain. This should include a suffix such as 'G' or 'M' to
+      indicate the units of the amount.
+    disks: list of strs of paths to disk images, in the order that
+      they should be attached to the guest. All disk images must be
+      owned by the user calling createDomain.
+    network: The name of the network to attach this VM to. The netwok
+      must have been created using
+      debmarshal.privops.networks.createNetwork by the user calling
+      createDomain.
+    mac: The MAC address of the new VM.
+    hypervisor: What hypervisor to use to start this VM. While it's
+      possible to mix hypervisors amongst the domains for a single
+      test suite, it is the caller's responsibility to keep track of
+      that when destroyDomain is called later. Currently only qemu is
+      supported.
+  """
+  pass
