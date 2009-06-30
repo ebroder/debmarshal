0f391f9df73446d2d2da8ee981caa298872ca22f
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-30 23:17:40 +0000

    Add tests for debmarshal.hypervisors.qemu.
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@85 485416fb-281f-0410-a025-fb7d5554b9fe

---
+++ testing/debmarshal/tests/hypervisors/test_qemu.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+# -*- python-indent: 2; py-indent-offset: 2 -*-
+# Copyright 2009 Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+"""tests for debmarshal.hypervisors.qemu."""
+
+__authors__ = [
+  'Evan Broder <ebroder@google.com>',
+]
+
+
+import os
+import unittest
+
+import mox
+
+from debmarshal.hypervisors import qemu
+from debmarshal import vm
+
+
+class TestQEMUDomainXML(mox.MoxTestBase):
+  """Test qemu.QEMU's domain XML generation."""
+  def test(self):
+    self.mox.StubOutWithMock(os, 'uname')
+    os.uname().MultipleTimes().AndReturn((
+      'Linux',
+      'hostname',
+      '2.6.24-24-generic',
+      '#1 SMP Wed Apr 15 18:53:17 UTC 2009',
+      'x86_64'))
+
+    self.mox.ReplayAll()
+
+    test_vm = vm.VM(name='some_name',
+                    memory=42,
+                    disks=[],
+                    network='debmarshal-0',
+                    mac='00:11:22:33:44:55')
+
+    xml = qemu.QEMU.domainXML(test_vm)
+
+    self.assertEqual(len(xml.xpath('/domain/os')), 1)
+    self.assertEqual(len(xml.xpath('/domain/os/type')), 1)
+    self.assertEqual(xml.xpath('string(/domain/os/type)'), 'hvm')
+    self.assertEqual(xml.xpath('string(/domain/os/type/@arch)'), 'x86_64')
+
+    self.assertEqual(len(xml.xpath('/domain/devices/emulator')), 1)
+    self.assertEqual(xml.xpath('string(/domain/devices/emulator)'),
+                     '/usr/bin/qemu-system-x86_64')
+
+
+if __name__ == '__main__':
+  unittest.main()
