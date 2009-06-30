3f32f3255a4a41f436d4614f8ec63416ddd3ed3a
evan.broder <evan.broder@485416fb-281f-0410-a025-fb7d5554b9fe>
2009-06-30 22:14:29 +0000

    Move runWithPrivilege, usage, main, and their helpers from debmarshal.privops into debmarshal.privops.utils.
    Also move their tests into debmarshal.tests.privops.test_utils.
    
    Signed-off-by: Evan Broder <ebroder@google.com>
    Signed-off-by: Drake Diedrich <dld@google.com>
    
    git-svn-id: https://debmarshal.googlecode.com/svn/trunk@67 485416fb-281f-0410-a025-fb7d5554b9fe

--- testing/debmarshal/privops/__init__.py
+++ testing/debmarshal/privops/__init__.py
@@ -47,93 +47,15 @@ try:
 except ImportError:  # pragma: no cover
   import pickle
 import re
-import subprocess
 import sys
 
-import decorator
 import ipaddr
 import libvirt
 from lxml import etree
 import virtinst
-import yaml
 
-from debmarshal import config
 from debmarshal import errors
-
-
-_SETUID_BINARY = os.path.join(config.libexecdir, 'debmarshpriv')
-
-
-_subcommands = {}
-
-
-def runWithPrivilege(subcommand):
-  """Decorator for wrapping a function to ensure that it gets run with
-  privileges by using a small setuid wrapper binary.
-
-  If a function wrapped with runWithPrivilege is called by a non-root
-  user, execute the setuid wrapper with the arguments passed in.
-
-  If a function is re-executed through the setuid wrapper, the
-  function arguments and keyword arguments are passed in through the
-  command line in YAML.
-
-  For security reasons, all YAML dumps and loads occur using the
-  "safe" parser, which will only (de-)serialize built-in types. This
-  means that arguments to and return values from functions wrapped in
-  runWithPrivilege must be limited to built-ins.
-
-  The return value or raised exceptions of the function are also
-  passed from the setuid subprocess back to the caller via standard
-  out, so functions wrapped in runWithPrivilege shouldn't print
-  anything.
-
-  Args:
-    subcommand: This is used as the first argument to the setuid
-      binary. Since the setuid binary simply executes this module, the
-      subcommand is also tracked internally for dispatching
-  """
-  @decorator.decorator
-  def _runWithPrivilege(f, *args, **kwargs):
-
-    # If we already have our privileges
-    if os.geteuid() == 0:
-      return f(*args, **kwargs)
-    else:
-
-      # Make sure that the setuid binary is actually setuid root so
-      # we don't get stuck in a loop
-      stats = os.stat(_SETUID_BINARY)
-      if not (stats.st_mode & 04000 and stats.st_uid == 0):
-        raise errors.Error('%s is not setuid root' % _SETUID_BINARY)
-
-      p = subprocess.Popen([_SETUID_BINARY,
-                            subcommand,
-                            yaml.safe_dump(args),
-                            yaml.safe_dump(kwargs)],
-                           stdin=None,
-                           stdout=subprocess.PIPE,
-                           close_fds=True)
-      rc = p.wait()
-
-      # This is the only place we don't use yaml.safe_load. That's
-      # intentional, because the source of this string is trusted, and
-      # may be an object like an exception.
-      ret = yaml.load(p.stdout)
-      if rc:
-        raise ret
-      else:
-        return ret
-
-  # The extra layer of redirection is needed if we want to both (a)
-  # use the decorator module (we do, because it gives us nice
-  # function-signature-preserving properties) and (b) associate a
-  # subcommand with the function it's wrapping at parse time.
-  def _makeRunWithPriv(f):
-    _subcommands[subcommand] = f
-    return _runWithPrivilege(f)
-
-  return _makeRunWithPriv
+from debmarshal.privops import utils
 
 
 _hostname_re = re.compile(r"([a-z0-9][a-z0-9-]{0,62}\.)+([a-z]{2,4})$", re.I)
@@ -274,7 +196,7 @@ def _genNetworkXML(name, gateway, netmask, hosts, dhcp):
   return etree.tostring(xml)
 
 
-@runWithPrivilege('create-network')
+@utils.runWithPrivilege('create-network')
 def createNetwork(hosts, dhcp=True):
   """All of the networking config you need for a debmarshal test rig.
 
@@ -377,7 +299,7 @@ def createNetwork(hosts, dhcp=True):
   return (net_name, net_gateway, net_mask, net_hosts)
 
 
-@runWithPrivilege('destroy-network')
+@utils.runWithPrivilege('destroy-network')
 def destroyNetwork(name):
   """Destroy a debmarshal network.
 
@@ -409,70 +331,5 @@ def destroyNetwork(name):
   _storeNetworkState(networks)
 
 
-def usage():
-  """Command-line usage information for debmarshal.privops.
-
-  Normal users are never expected to trigger this, because normal
-  users are never supposed to run debmarshal.privops directly; instead
-  other debmarshal scripts should use functions in this module, which
-  results in the setuid re-execution.
-
-  But just in case someone runs it directly, we'll tell them what it
-  does.
-  """
-  print >>sys.stderr, ("Usage: %s subcommand args kwargs" %
-                       os.path.basename(sys.argv[0]))
-  print >>sys.stderr
-  print >>sys.stderr, "  args is a YAML-encoded list"
-  print >>sys.stderr, "  kwargs is a YAML-encoded dict"
-
-
-def main(args):
-  """Dispatch module invocations.
-
-  A sort of other half of the runWithPrivilege decorator, main parses
-  the arguments and kwargs passed in on the command line and calls the
-  appropriate function. It also intercepts any raised exceptions or
-  return values, serializes them, and passes them over standard out to
-  whatever invoked the module.
-
-  Note: this doesn't intercept exceptions raised as part of the
-  initial argument parsing, because we're optimistically assuming that
-  arguments that come in from runWithPrivilege are flawless
-  (heh...). Those exceptions will get rendered by the Python
-  interpreter to standard error, as will any errors that we generate.
-
-  Args:
-    args, a list of arguments passed in to the module, not including
-      argv[0]
-
-  Returns:
-    Return an integer, which becomes the exit code for when the module
-      is run as a script.
-  """
-  if len(args) != 3:
-    usage()
-    return 1
-
-  subcommand, posargs, kwargs = args
-  posargs = yaml.safe_load(posargs)
-  kwargs = yaml.safe_load(kwargs)
-
-  priv_func = _subcommands[subcommand]
-
-  try:
-    ret = priv_func(*posargs, **kwargs)
-    rc = 0
-  except Exception, e:
-    ret = e
-    rc = 1
-
-  # This is the only place we don't use yaml.safe_dump, because this
-  # output is trusted when it gets parsed, and we want to be able to
-  # pass around arbitrary objects
-  print yaml.dump(ret)
-  return rc
-
-
 if __name__ == '__main__':  # pragma: no cover
-  sys.exit(main(sys.argv[1:]))
+  sys.exit(utils.main(sys.argv[1:]))
---
+++ testing/debmarshal/tests/privops/test_utils.py
@@ -0,0 +1,222 @@
+#!/usr/bin/python
+# -*- python-indent: 2; py-indent-offset: 2 -*-
+# Copyright 2009 Google Inc.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA.
+"""tests for debmarshal.privops.utils"""
+
+__authors__ = [
+    'Evan Broder <ebroder@google.com>',
+]
+
+
+import os
+import posix
+try:
+  import cStringIO as StringIO
+except ImportError:
+  import StringIO
+import subprocess
+import sys
+import unittest
+
+import mox
+import yaml
+
+from debmarshal import errors
+from debmarshal.privops import utils
+
+
+class TestRunWithPrivilege(mox.MoxTestBase):
+  """Testing runWithPrivilege before it actually executes anything"""
+  def testFunctionCalledDirectlyAsRoot(self):
+    """Test that functions wrapped in runWithPrivilege are executed
+    directly if you're already root.
+    """
+    self.mox.StubOutWithMock(os, 'geteuid')
+    os.geteuid().AndReturn(0)
+
+    # We want to make sure that runWithPrivilege doesn't try to
+    # re-execute itself through the setuid wrapper.
+    #
+    # By stubbing out os.stat without actually recording any calls to
+    # it, we can trigger an error of runWithPrivilege starts trying to
+    # re-execute itself.
+    self.mox.StubOutWithMock(os, 'stat')
+    self.mox.StubOutWithMock(subprocess, 'Popen')
+
+    self.mox.ReplayAll()
+
+    @utils.runWithPrivilege('test')
+    def func():
+      # Stupid function - all we care about is that it gets run
+      return 1
+
+    self.assertEqual(func(), 1)
+
+  def testAbortIfWrapperNotSetuid(self):
+    """Test that if you're not root, and the setuid wrapper isn't
+    setuid, runWithPrivileges aborts.
+    """
+    self.mox.StubOutWithMock(os, 'geteuid')
+    os.geteuid().AndReturn(1000)
+
+    self.mox.StubOutWithMock(os, 'stat')
+    os.stat(utils._SETUID_BINARY).AndReturn(posix.stat_result([
+        # st_mode is the first argument; we don't care about anything
+        # else
+        0755, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+
+    self.mox.ReplayAll()
+
+    @utils.runWithPrivilege('test')
+    def func():
+      self.fail('This function should never have been run.')
+
+    self.assertRaises(errors.Error, func)
+
+class TestReexecResults(mox.MoxTestBase):
+  """Test the actual process of execing the setuid wrapper.
+
+  This requires some substantial mocking of both the os module and
+  subprocess.Popen.
+  """
+  def setUp(self):
+    """Setup mocks through the point of actually forking and execing.
+
+    This involves faking a binary that's setuid, as well as faking
+    actually running it.
+    """
+    super(TestReexecResults, self).setUp()
+
+    self.mox.StubOutWithMock(os, 'geteuid')
+    os.geteuid().AndReturn(1000)
+
+    self.mox.StubOutWithMock(os, 'stat')
+    os.stat(utils._SETUID_BINARY).AndReturn(posix.stat_result([
+          # st_mode
+          04755,
+          0, 0, 0,
+          # uid
+          0,
+          0, 0, 0, 0, 0]))
+
+    self.mock_popen = self.mox.CreateMock(subprocess.Popen)
+    self.mox.StubOutWithMock(subprocess, 'Popen', use_mock_anything=True)
+    subprocess.Popen([utils._SETUID_BINARY,
+                      'test',
+                      mox.Func(lambda x: yaml.safe_load(x) == ['a', 'b']),
+                      mox.Func(lambda x: yaml.safe_load(x) == {'c': 'd'})],
+                     stdin=None,
+                     stdout=subprocess.PIPE,
+                     close_fds=True).AndReturn(self.mock_popen)
+
+  def testReexecSuccessResult(self):
+    """Verify that the printout from runWithPrivileges is treated as a
+    return value if the program exits with a return code of 0.
+    """
+    self.mock_popen.wait().AndReturn(0)
+    self.mock_popen.stdout = StringIO.StringIO(yaml.dump('foo'))
+
+    self.mox.ReplayAll()
+
+    @utils.runWithPrivilege('test')
+    def func(*args, **kwargs):
+      return 'foo'
+
+    self.assertEquals(func('a', 'b', c='d'), 'foo')
+
+  def testReexecFailureResult(self):
+    """Verify that the printout from runWithPrivileges is treated as
+    an exception if the program exits with a return code of 1.
+    """
+    self.mock_popen.wait().AndReturn(1)
+    self.mock_popen.stdout = StringIO.StringIO(yaml.dump(Exception('failure')))
+
+    self.mox.ReplayAll()
+
+    @utils.runWithPrivilege('test')
+    def func(*args, **kwargs):
+      raise Exception('failure')
+
+    self.assertRaises(Exception, lambda: func('a', 'b', c='d'))
+
+
+class TestUsage(mox.MoxTestBase):
+  """Make sure that usage information gets printed"""
+  def setUp(self):
+    """Record printing out something that looks like usage information.
+
+    We'll try to trigger it a bunch of different ways.
+    """
+    super(TestUsage, self).setUp()
+
+    self.mox.StubOutWithMock(sys, 'stderr')
+    sys.stderr.write(mox.StrContains('Usage'))
+    sys.stderr.write(mox.IgnoreArg()).MultipleTimes()
+
+    self.mox.ReplayAll()
+
+  def testNoArgs(self):
+    """Trigger usage information by passing in no arguments"""
+    self.assertEqual(utils.main([]), 1)
+
+  def testTooManyArgs(self):
+    """Trigger usage information by passing in too many arguments"""
+    self.assertEqual(utils.main(['a', 'b', 'c', 'd']), 1)
+
+
+class TestMain(mox.MoxTestBase):
+  """Test argument and return parsing in main"""
+  def testSuccess(self):
+    """Test that the return value is printed to stdout and that the
+    return code is 0 if the function returns
+    """
+    self.mox.StubOutWithMock(sys, 'stdout')
+    print >>sys.stdout, yaml.dump('success')
+
+    self.mox.ReplayAll()
+
+    args = ('foo', {'bar': 'quux'})
+    kwargs = {'spam': 'eggs'}
+
+    def test_args(*input_args, **input_kwargs):
+      self.assertEqual(args, input_args)
+      self.assertEqual(kwargs, input_kwargs)
+      return 'success'
+    utils._subcommands['test'] = test_args
+
+    self.assertEqual(utils.main([
+          'test', yaml.safe_dump(args), yaml.safe_dump(kwargs)]), 0)
+
+  def testFailure(self):
+    """Test that the exception is printed to stdout and that the
+    return code is 1 if the function raises one
+    """
+    self.mox.StubOutWithMock(sys, 'stdout')
+    print >>sys.stdout, yaml.dump(Exception('failure'))
+
+    self.mox.ReplayAll()
+
+    def test():
+      raise Exception('failure')
+    utils._subcommands['test'] = test
+
+    self.assertEqual(utils.main([
+          'test', yaml.safe_dump([]), yaml.safe_dump({})]), 1)
+
+
+if __name__ == '__main__':
+  unittest.main()
