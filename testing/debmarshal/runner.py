d74ab32195cb924d89f36590863095e2d1d6646f
Evan Broder <ebroder@google.com>
2009-08-16 19:58:59 -0700

    In debmarshal.runner.prepareTest, spin up a thread to oversee installation for each VM, and then wait for them all to complete.
    Signed-off-by: Evan Broder <ebroder@google.com>

--- testing/debmarshal/runner.py
+++ testing/debmarshal/runner.py
@@ -32,9 +32,15 @@ __authors__ = [
 
 import optparse
 import os
+import Queue
+import subprocess
 import sys
+import threading
 
 import yaml
+
+from debmarshal.distros import base
+from debmarshal import privops
 
 
 USAGE = """Usage: debmarshal prepare [--options] [<test1> [<test2> ...]]"""
@@ -64,6 +70,55 @@ def parsePrepareArgs(argv):
   return parser.parse_args(argv)
 
 
+def prepareSummary(results_queue, stream=sys.stdout):
+  """Given a queue of results from installations, print out a summary.
+
+  This prints out a unittest-style summary of the installs that
+  were logged in the passed-in results queue.
+
+  It's the caller's responsibility to make sure that no other threads
+  are reading from or writing to the results_queue while this function
+  is running.
+
+  Args:
+    results_queue: A Queue.Queue containing the results of a series of
+      VM installations. Each element in the queue should be of the
+      form (test, vm_name, passed, error), where error is None if
+      there was no error.
+    stream: The stream to print the results to.
+
+  Returns:
+    The exit code for the preparation program - 1 if there were
+      failures; 0 if there were none.
+  """
+  result_count = 0
+  fail_count = 0
+  while not results_queue.empty():
+    result = results_queue.get()
+    result_count += 1
+
+    stream.write('Prepared %s ...' % result[1])
+    if result[2]:
+      stream.write('OK\n')
+    else:
+      fail_count += 1
+      stream.write('F\n')
+
+      stream.write(result[3])
+      stream.write('\n')
+
+  stream.write('\n')
+  stream.write('%d VM%s built for %s\n' % (
+      result_count,
+      's' if result_count != 1 else '',
+      test))
+
+  if fail_count:
+    stream.write('(%d failed)\n' % fail_count)
+
+  return int(bool(fail_count))
+
+
 def prepareTest(test):
   """Prepare the disk image for a single test.
 
@@ -86,7 +141,33 @@ def prepareTest(test):
     web_port = httpd.stdout.read()
 
     try:
-      pass
+      results_queue = Queue.Queue()
+      threads = []
+
+      # For each VM that needs to be installed, create a thread to run
+      # the installer
+      for vm in vms:
+        dist = base.findDistribution(config['vms'][vm]['distribution'])
+
+        threads.append(threading.Thread(
+            target=dist,
+            args=(test,
+                  vm,
+                  net_name,
+                  net_gate,
+                  net_vms[vm][1],
+                  web_port,
+                  results_queue)))
+
+      # Ready, set, go!
+      for t in threads:
+        t.start()
+
+      # Wait until all of the threads have completed execution
+      for t in threads:
+        t.join()
+
+      return prepareSummary(results_queue)
     finally:
       httpd.terminate()
   finally:
